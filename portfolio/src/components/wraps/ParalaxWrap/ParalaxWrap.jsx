0fa90ee34e64e6abd4d715e80c844ed42d1b78ce
garry1980 <ludingv@yandex.ru>
2020-01-18 23:43:34 +0500

    Add parallax

--- portfolio/src/components/wraps/ParalaxWrap/ParalaxWrap.jsx
+++ portfolio/src/components/wraps/ParalaxWrap/ParalaxWrap.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 // Components
 import ParalaxEl from '../../elements/ParalaxEl/ParalaxEl';
@@ -27,17 +27,30 @@ const sidebarBgImgs = [
 ];
 
 const ParalaxWrap = () => {
+
+    const [X, setX] = useState(0);
+    const [Y, setY] = useState(0);
+
+    useEffect(() => {
+        window.addEventListener('mousemove', (e) => {
+            setX(((window.screen.width / 2) - e.pageX) * 0.015);
+            setY(((window.screen.width / 2) - e.pageY) * 0.015);
+        })
+    }, [])
+
     return (
         <div className="paralax">
             {sidebarBgImgs.map((sidebarBgImg, i) => (
-                <span className="paralax__wrap">
-                    {sidebarBgImg.map((el) => (
+                <div
+                className="paralax__wrap"
+                key={i} style={{transform: `translate(${X * (i === 0 ? i + 1 : i * 2.5)}px, ${Y * (i === 0 ? i + 1 : i * 2.5)}px)`}}>
+                    {sidebarBgImg.map((el, i) => (
                         <ParalaxEl 
                         {...el}
                         key={el.id}
                         />
                     ))}        
-                </span>
+                </div>
             ))}
         </div>
     );
